2e4173ac214fa6af8d89bc2762b1cfc61d566c73
ArteyaLutchman <93239880+ArteyaLutchman@users.noreply.github.com>
2025-01-20 23:17:40 -0500

    Finished product

--- Assets/Level/Scripts/FPSController.cs
+++ Assets/Level/Scripts/FPSController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
-
 public class FPSController : MonoBehaviour
 {
     public Camera playerCamera;
@@ -14,6 +13,10 @@ public class FPSController : MonoBehaviour
 
     public float lookSpeed = 2f;
     public float lookXLimit = 45f;
+
+    public float crouchSpeed = 1f; // Speed while crouching
+    public float crouchHeight = 1f; // Height when crouched
+    public float normalHeight = 2f; // Normal height
 
     Vector3 moveDirection = Vector3.zero;
     float rotationX = 0;
@@ -28,7 +31,6 @@ public class FPSController : MonoBehaviour
         characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        
     }
 
     // Update is called once per frame
@@ -39,15 +41,29 @@ public class FPSController : MonoBehaviour
         Vector3 right = transform.TransformDirection(Vector3.right);
 
         // Press Left Shift to run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        bool isRunning = Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl);
+        bool isCrouching = Input.GetKey(KeyCode.LeftControl);
+        float currentSpeed = isCrouching ? crouchSpeed : (isRunning ? runSpeed : walkSpeed);
+
+        float curSpeedX = canMove ? currentSpeed * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? currentSpeed * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
+
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        // Adjust height for crouching
+        if (isCrouching)
+        {
+            characterController.height = Mathf.Lerp(characterController.height, crouchHeight, Time.deltaTime * 10);
+        }
+        else
+        {
+            characterController.height = Mathf.Lerp(characterController.height, normalHeight, Time.deltaTime * 10);
+        }
         #endregion
 
         #region Handles Jumping
-        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded && !isCrouching)
         {
             moveDirection.y = jumpPower;
         }
@@ -60,7 +76,6 @@ public class FPSController : MonoBehaviour
         {
             moveDirection.y -= gravity * Time.deltaTime;
         }
-
         #endregion
 
         #region Handles Rotation
@@ -73,8 +88,6 @@ public class FPSController : MonoBehaviour
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
-
         #endregion
-
     }
 }
